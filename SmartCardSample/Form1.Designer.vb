4900512cf6818387cfb8f5dcbf5cb751e7bf0657
dechnostick <etherealmaro@gmail.com>
2014-08-12 00:24:09 +0900

    最初のコミット

---
+++ SmartCardSample/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(13, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(385, 147)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 12!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(409, 174)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ SmartCardSample/Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Text
+Imports GemCard
+
+Public Class Form1
+
+    Private reader As String
+    Private text As String
+    Protected Friend WithEvents smartCard As CardBase
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        smartCard = New CardNative()
+        reader = smartCard.ListReaders()(0)
+        smartCard.StartCardEvents(reader)
+        Msg("Reader: " & reader)
+        Timer1.Start()
+    End Sub
+
+    Private Sub smartCard_OnCardInserted() Handles smartCard.OnCardInserted
+        text = ""
+        Msg("カード読み取り中")
+        Try
+            smartCard.Connect(reader, SHARE.Shared, PROTOCOL.T0orT1)
+        Catch ex As Exception
+            Msg("接続失敗")
+        End Try
+
+        Try
+            Dim atrValue As Byte() = smartCard.GetAttribute(SCARD_ATTR_VALUE.ATR_STRING)
+            Msg(ByteArrayToString(atrValue) & "(winscard:ATR)")
+            Msg(ToIDm(atrValue) & "(winscard:IDm)")
+        Catch ex As Exception
+            Msg("読み取り失敗")
+        End Try
+
+        Try
+            smartCard.Disconnect(DISCONNECT.Unpower)
+        Catch ex As Exception
+            Msg("切断失敗")
+        End Try
+    End Sub
+
+    Private Sub smartCard_OnCardRemoved() Handles smartCard.OnCardRemoved
+        Msg("カード抜き取り完了")
+    End Sub
+
+    Private Shared Function ByteArrayToString(ByVal data As Byte()) As String
+        Dim sb As New StringBuilder
+        For i As Integer = 0 To data.Length - 1
+            sb.AppendFormat("{0:X02}", data(i))
+        Next
+        Return sb.ToString()
+    End Function
+
+    Private Shared Function ToIDm(ByVal data As Byte()) As String
+        Dim sb As New StringBuilder
+        ' 3B 8C 80 01 04 43 FD
+        If data.Length = 17 Then
+            For nI As Integer = 7 To 14
+                sb.AppendFormat("{0:X02}", data(nI))
+            Next
+        End If
+        Return sb.ToString()
+    End Function
+
+    Private Sub Msg(str As String)
+        text &= str & vbNewLine
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        smartCard.Disconnect(DISCONNECT.Unpower)
+        smartCard.StopCardEvents()
+        e.Cancel = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        TextBox1.Text = text
+    End Sub
+End Class
